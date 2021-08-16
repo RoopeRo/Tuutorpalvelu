708022a32cc0b077d60efd4dd29e3d0ef818ebe2
Niina Siitari <siitari.niina@gmail.com>
2021-08-16 14:06:03 +0300

    Scaffoldattu modelsit

--- WebApplication1/DataAccess.cs
+++ WebApplication1/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,6 @@ namespace WebApplication1
 {
     public class DataAccess
     {
-
 
     }
 }
