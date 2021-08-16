81daa12346d8cc6e073910f919d4244a120eaec6
Marko S <msalmikangas@gmail.com>
2021-08-16 15:04:26 +0300

    Käyttäjätaulu lisätty

---
+++ WebApplication1/Models/Käyttäjä.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApplication1.Models
+{
+    public partial class Käyttäjä
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
