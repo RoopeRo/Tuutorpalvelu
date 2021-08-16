446c6f954835a8250aee86e65d91ee6fdb219539
Niina Siitari <siitari.niina@gmail.com>
2021-08-16 16:44:36 +0300

    Kontrollit pohjat tehty

---
+++ WebApplication1/Controllers/PalveluController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class PalveluController : Controller
+    {
+        public IActionResult HaePalvelutKaikki()
+        {
+            DataAccess haku = new DataAccess();
+            var palvelut = haku.haepalvelut();
+            ViewBag.palvelut = palvelut;
+            return View();
+        }
+
+        public IActionResult HaeDetails()
+        {
+            //detailsien näyttämien
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult HaePalvelutFiltteri()
+        {
+            return View();
+        }
+    }
+}
