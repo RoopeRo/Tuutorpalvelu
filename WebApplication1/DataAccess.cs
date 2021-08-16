b6ceb33ce50a029454809f068f8667eede4dce83
Roope Rouvali <roope.rouvali@awacademyconsultant.fi>
2021-08-16 14:30:19 +0300

    hae tuutorit, tuutorien palvelut, haepalvelut

--- WebApplication1/DataAccess.cs
+++ WebApplication1/DataAccess.cs
@@ -3,11 +3,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
     public class DataAccess
     {
+        TutorpalveluDBContext db = new TutorpalveluDBContext();
+
+        public List<Palvelu> haepalvelut()
+        {
+
+            var Lista = db.Palvelus.Where(p => p.PalveluId != -1);
+
+            return (List<Palvelu>)Lista;
+        }
+
+        public List<Person> haetuutorit()
+        {
+            var lista = db.People.Where(p => p.Tutor == true);
+
+            return (List<Person>)lista;
+        }
+
+        public List<Person> haetuutorinpalvelut(int tunniste)
+        {
+            var lista = db.Palvelus.Where(p => p.TutorId == tunniste);
+
+            return (List<Person>)lista;
+        }
+
+
 
     }
 }
