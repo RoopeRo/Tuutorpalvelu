b6a86754ff8dfcf1f67bf487dbb460585cd2e5f5
Niina Siitari <siitari.niina@gmail.com>
2021-08-16 15:46:13 +0300

    Kontrollien pohjat

---
+++ WebApplication1/Controllers/PalveluController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class PalveluController : Controller
+    {  
+        [HttpGet]
+        public IActionResult HaePalvelut()
+        {
+            DA haku = new DA();
+            var palvelut = haku.HaePalvelut();
+            ViewBag.Palvelut = palvelut;
+            return View();
+        }
+
+        public IActionResult NäytäPalvelu()
+        {
+            //Detailsit palvelusta
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/TutorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class TutorController : Controller
+    {
+        [HttpPost]
+        public IActionResult LisääTutor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LisääPalvelu()
+        {
+            return View();
+        }
+    }
+}
