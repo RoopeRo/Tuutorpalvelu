eddb62bbb5c59e5447a3c026e613015d040c8d52
msalmika <77154586+msalmika@users.noreply.github.com>
2021-08-16 13:38:43 +0300

    Add files via upload

---
+++ DummyData.sql
@@ -0,0 +1,13 @@
+insert into Person values('Mikki','Hiiri', 'Ankkalinnankatu 12','Ankkalinna','05400','0404040404','mh@ankka.fi','1');
+insert into Person values('Hessu','Hopo', 'Ankkalinnankatu 10','Ankkalinna','05400','0404040404','hh@ankka.fi','2');
+insert into Person values('Minni','Hiiri', 'Ankkalinnankatu 12','Ankkalinna','05400','0404040404','mhi@ankka.fi','3');
+
+
+
+insert into Palvelu values('Yksityisetsivän palvelut','Tutkimus','Etsintä',10.0,'2020-10-24 11:00', 60,'ankkalinna','0',1)
+insert into Palvelu values('Sukan kudonta','Käsityöt','Villasukat',10.0,'2007-10-24 11:00:00',120,'ankkalinna','0',2)
+insert into Palvelu values('Kadonneen aarteen metsästäjät','Tutkimus','Etsintä',10.0,'2021-10-24 13:00:00',30,'ankkalinna','0',1)
+
+select * from Person;
+select * from Palvelu;
+
