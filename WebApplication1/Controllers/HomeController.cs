d63a04eb958719210ea7c893ef5bbaaadb26dbd0
Marko S <msalmikangas@gmail.com>
2021-08-19 15:01:31 +0300

    Sivun kaatumista estetty

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -88,6 +88,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult SivuKaatuu()
+        {
+            return View();
+        }
         public IActionResult Uloskirjaus()
         {
             HttpContext.Session.Clear();
